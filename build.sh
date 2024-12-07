27605a283275ef769c1665f7937a17d553d0062c
Vadim Babadzhanyan <vadim.babadzhanyan@my.games>
2024-12-08 01:38:27 +0300

    no message

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 
 # docker buildx build --tag akeb/monitoring-worker --platform linux/amd64,linux/arm/v7,linux/arm64/v8 ./
 
-
-
 docker build \
   --build-arg WORKER_VERSION=local \
   --tag akeb/monitoring-worker:local \
